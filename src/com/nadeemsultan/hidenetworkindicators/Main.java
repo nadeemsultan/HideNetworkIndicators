d9fafa2847ad7413a85a3a23b90576138c4ac31d
Nadeem Sultan <nadeemasultan@gmail.com>
2014-08-03 00:23:33 -0400

    Fix some bugs

--- src/com/nadeemsultan/hidenetworkindicators/Main.java
+++ src/com/nadeemsultan/hidenetworkindicators/Main.java
@@ -2,19 +2,11 @@ package com.nadeemsultan.hidenetworkindicators;
 
 import android.content.res.XModuleResources;
 import de.robv.android.xposed.IXposedHookInitPackageResources;
-import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class Main implements IXposedHookZygoteInit, IXposedHookInitPackageResources, IXposedHookLoadPackage {
+public class Main implements IXposedHookZygoteInit, IXposedHookInitPackageResources {
     private static String MODULE_PATH = null;
-    
-    //Log - Remove before release
-    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
-    }
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
@@ -32,26 +24,3 @@ public class Main implements IXposedHookZygoteInit, IXposedHookInitPackageResour
         resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_out", modRes.fwd(R.drawable.wifi_activity));
     }
 }
-
-
-
-
-/*public class Main implements IXposedHookLoadPackage {
-	ImageView networkActivity;
-	
-	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
-		if (!resparam.packageName.equals("com.android.systemui")) {
-			return;
-		}
-		resparam.res.hookLayout("com.android.systemui","layout","signal_cluster_view",new XC_LayoutInflated() {
-			@Override
-			public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-				networkActivity = (ImageView) liparam.view.findViewById(liparam.res.getIdentifier("wifi_inout", "id" , "com.android.systemui"));
-				
-			}
-		});
-	}
-
-	
-
-}*/
